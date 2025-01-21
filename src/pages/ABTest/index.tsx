686f55b7fb1123a730184e31e6a12ed386a639d9
gengkk <gengkk@hinadt.com>
2025-01-21 11:11:17 +0800

    hina-cloud-js-sdk demo

--- src/pages/ABTest/index.tsx
+++ src/pages/ABTest/index.tsx
@@ -1,5 +1,6 @@
 import {Button, Flex, message} from 'antd';
 import React, {useEffect, useState} from 'react';
+import Mock from 'mockjs'
 
 const ABTest: React.FC = () => {
 
@@ -23,13 +24,54 @@ const ABTest: React.FC = () => {
    * 上报【立即购买】事件
    */
   const commitRoombuyNowClick = () => {
-    window["hina"].track('roombuyNowClick', {commodity_id: "1", commodity_name: '洗发水', present_price: 10});
+    window["hina"].track('roombuyNowClick',
+      Mock.mock({
+        'commodity_id': '@integer(10, 100000)',
+        'commodity_name': '@cword(5)',
+        'present_price': '@integer(10, 100000)',
+        strList: Mock.mock(['@ctitle()', '@ctitle()', '@ctitle()']),
+        obj: Mock.mock({
+          'id': '@integer(10, 100000)',
+          'name': '@name',
+          'age|20-30': 25,
+          'gender|1': ['男', '女']
+        }),
+      }));
     message.success('购买成功', 0.2);
   };
 
 
   return (
     <Flex gap="small" wrap>
+      <Button type="primary" onClick={() => {
+        window["hina"].track('H_AppStart', {
+          obj: Mock.mock({
+            'id': '@integer(10, 100000)',
+            'name': '@name',
+            'age|20-30': 25,
+            'gender|1': ['男', '女']
+          }),
+          strList: Mock.mock(['@ctitle()', '@ctitle()', '@ctitle()']),
+          H_is_first_time: Mock.mock('@boolean()'),
+          H_screen_name: Mock.mock('@ctitle()'),
+          H_title: Mock.mock('@ctitle()')
+        })
+        ;
+      }}>App 启动</Button>
+      <Button type="primary" onClick={() => {
+        window["hina"].track('inRoom', {
+          room_name: Mock.mock('@cword(5)'),
+          player_name: Mock.mock('@cname()'),
+          is_follow_anchor: Mock.mock('@boolean()'),
+          strList: Mock.mock(['@ctitle()', '@ctitle()', '@ctitle()']),
+          obj: Mock.mock({
+            'id': '@integer(10, 100000)',
+            'name': '@name',
+            'age|20-30': 25,
+            'gender|1': ['男', '女']
+          }),
+        });
+      }}>进入直播间</Button>
       <Button type="primary" style={{backgroundColor: btnColor}} onClick={() => {
         commitRoombuyNowClick();
       }}>立即购买</Button>
